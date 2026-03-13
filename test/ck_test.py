a32a97805523e837936ff757d5c086a9b97313d2
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2026-03-13 19:57:36 +0300

    Написан новый автотест

---
+++ test/ck_test.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def test_put_complet_true():
+    """Создать задачу, проставить отметку о выполнении и проверить что completed ==True"""
+    body = {"title": "ck", "completed": False}
+    # Создаём задачу
+    response = requests.post(url="https://todo-app-sky.herokuapp.com/", json=body)
+    id = response.json()["id"]
+
+    # Редактируем созданную ранее задачу
+    body = {"completed": True}
+    response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
+    assert response.status_code == 200
+
+    # Проверяем что completed ==True после изменения
+    response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
+    assert response.status_code == 200
+    assert response.json()['completed'] == True
